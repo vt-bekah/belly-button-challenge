3d6a05198083b09c89d8fac075a48b387a4c2134
vt-bekah <vt_bekah@hotmail.com>
2023-09-16 11:51:54 -0500

    add chart creation functions for single function set

--- static/js/app.js
+++ static/js/app.js
@@ -25,20 +25,84 @@ function init() {
       dropdownMenu.append("option").text(id).property("value", id)
     })
 
-    // Grab data for charts
-    let sampleValues = data.samples.map(value => value.sample_values)[0]
-    console.log("bar chart data: )", sampleValues)
-    let otuIDs = data.samples.map(value => value.otu_ids)[0]
-    console.log("bar chart labels: )", otuIDs)
-    let outLabels = data.samples.map(value => value.otu_labels)[0]
-    console.log("bar chart hover: )", outLabels)
+    // // Grab the chart data
+    // let sampleValues = data.samples.map(value => value.sample_values)[0]
+    // console.log("bar chart data: )", sampleValues)
+    // let otuIDs = data.samples.map(value => value.otu_ids)[0]
+    // console.log("bar chart labels: )", otuIDs)
+    // let outLabels = data.samples.map(value => value.otu_labels)[0]
+    // console.log("bar chart hover: )", outLabels)
     
-    // Grab the demographic data
-    let demographicData = data.metadata[0]
-    console.log("demographic info: )", demographicData)
+    // // Grab the demographic data
+    // let demographicData = data.metadata[0]
+    // console.log("demographic info: )", demographicData)
 
   })
 }
 
+// Build the horizontal bar chart
+function buildHBarChart(sample) {
+  
+  d3.json(url).then(function(data) {
+  
+      // Grab the chart data
+
+      let sampleData = data.samples[0]
+
+      let sampleValues = sampleData.sample_values
+      console.log("bar chart data: )", sampleValues)
+      let otuIDs = sampleData.otu_ids
+      console.log("bar chart labels: )", otuIDs)
+      let otuLabels = sampleData.otu_labels
+      console.log("bar chart hover: )", otuLabels)
+      
+      barData = [{
+        x: sampleValues.slice(0,10).reverse(),
+        y: otuIDs.slice(0,10).map(id => `OTU ${id}`).reverse(),
+        text: otuLabels.slice(0,10).reverse(),
+        type: "bar",
+        orientation: "h"
+      }];
+    
+      Plotly.newPlot("bar", barData);
+  
+    })
+}
+
+// Build the bubble chart
+function buildHBubbleChart(sample) {
+  
+  d3.json(url).then(function(data) {
+  
+      // Grab the chart data
+
+      let sampleData = data.samples[0]
+
+      let sampleValues = sampleData.sample_values
+      console.log("bar chart data: )", sampleValues)
+      let otuIDs = sampleData.otu_ids
+      console.log("bar chart labels: )", otuIDs)
+      let otuLabels = sampleData.otu_labels
+      console.log("bar chart hover: )", otuLabels)
+      
+      bubbleData = [{
+        x: otuIDs,
+        y: sampleValues,
+        text: otuLabels,
+        mode: "markers",
+        marker: {
+          size: sampleValues,
+          color: otuIDs,
+          colorscale: "Earth"
+        }
+      }];
+    
+      Plotly.newPlot("bubble", bubbleData);
+  
+  })
+}
+
 init()
+buildHBarChart()
+buildHBubbleChart()
 
