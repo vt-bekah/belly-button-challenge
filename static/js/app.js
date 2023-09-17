ab2c218a2be2fa45e1b36f44ce56194cb5b5fa7e
vt-bekah <vt_bekah@hotmail.com>
2023-09-17 15:37:45 -0500

    added chart axis labels and cleaned up comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,22 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+// Console log promise for debug purposes
+const dataPromise = d3.json(url)
+console.log("Data Promise: ", dataPromise)
 
-// Fetch the JSON data and console log it
+// Fetch the JSON data and console log it for debug purposes
 d3.json(url).then(function(data) {
-  console.log("data", data);
-});
+  console.log("data", data)
+})
 
-// Initializes the page with a default plot using d3
+// Initializes the page with a default plot
 function init() {
   
-  // Select the dropdown Menu
-  let dropdownMenu = d3.select("#selDataset");
+  // Update the dropdown menu based on the data
+  // Select the dropdown menu
+  let dropdownMenu = d3.select("#selDataset")
   
   d3.json(url).then(function(data) {
-    
     // Store the subject IDs in a list (console log for debug)
     let dropdownList = data.names
     console.log("DD List: ", dropdownList)
@@ -25,7 +25,7 @@ function init() {
       dropdownMenu.append("option").text(id).property("value", id)
     })
 
-    // Grab the initialization chart data
+    // Grab the initialization chart data (console log for debug)
     let sampleZero = data.metadata[0].id
     console.log("sample zero", sampleZero)
 
@@ -38,9 +38,9 @@ function init() {
 }
 
 // Captures a change in dropdown and updates charts and tables
-d3.selectAll("#selDataset").on("change", updateChartsTable);
+d3.selectAll("#selDataset").on("change", updateChartsTable)
 
-// Update the restyled plot's values
+// Function to update the charts based on value change in dropdown menu
 function updateChartsTable() {
   // Use D3 to select the dropdown menu
   let ddMenu = d3.select("#selDataset")
@@ -49,33 +49,31 @@ function updateChartsTable() {
   console.log("new sample", newSample)
 
   // Update charts 
-  // Plotly.restyle("bar", "values", [newSample])
-  // Plotly.restyle("bubble", "values", [newSample])
   buildHBarChart(newSample)
   buildHBubbleChart(newSample)
 
   // Delete old demographics info and populate new
-  let demoBody = d3.select('#sample-metadata')
+  let demoBody = d3.select("#sample-metadata")
   demoBody.selectAll("p").remove()
   buildDemographics(newSample)
 }
 
-// Build the horizontal bar chart
+// Function to build the horizontal bar chart
 function buildHBarChart(sample) {
   
   d3.json(url).then(function(data) {
   
       // Grab all the chart data
       let sampleDataAll = data.samples
-      // Grab the chart data
+      // Filter the data based on the sample input
       let sampleData = sampleDataAll.filter(dData => dData.id == sample)[0]
-
+      // Set variables to contain the arrays required to create the chart information (console log for debug)
       let sampleValues = sampleData.sample_values
-      console.log("bar chart data: )", sampleValues)
+      console.log("bar chart data:", sampleValues)
       let otuIDs = sampleData.otu_ids
-      console.log("bar chart labels: )", otuIDs)
+      console.log("bar chart labels:", otuIDs)
       let otuLabels = sampleData.otu_labels
-      console.log("bar chart hover: )", otuLabels)
+      console.log("bar chart hover:", otuLabels)
       
       // set the chart trace data
       barData = [{
@@ -84,11 +82,15 @@ function buildHBarChart(sample) {
         text: otuLabels.slice(0,10).reverse(),
         type: "bar",
         orientation: "h"
-      }];
+      }]
+      
+      // set layout for bubble chart
+      let layout = {
+        xaxis:{title: "Count of Bacteria"}
+      }
       
       // plot in the designated spot in index.html
-      Plotly.newPlot("bar", barData);
-  
+      Plotly.newPlot("bar", barData, layout)
     })
 }
 
@@ -99,15 +101,15 @@ function buildHBubbleChart(sample) {
 
       // Grab all the chart data
       let sampleDataAll = data.samples
-      // Grab the chart data
+      // Filter the data based on the sample input
       let sampleData = sampleDataAll.filter(dData => dData.id == sample)[0]
-
+      // Set variables to contain the arrays required to create the chart information (console log for debug)
       let sampleValues = sampleData.sample_values
-      console.log("bubble chart data: )", sampleValues)
+      console.log("bubble chart data:", sampleValues)
       let otuIDs = sampleData.otu_ids
-      console.log("bubble chart labels: )", otuIDs)
+      console.log("bubble chart labels:", otuIDs)
       let otuLabels = sampleData.otu_labels
-      console.log("bubble chart hover: )", otuLabels)
+      console.log("bubble chart hover:", otuLabels)
       
       // set the chart trace data
       bubbleData = [{
@@ -120,11 +122,16 @@ function buildHBubbleChart(sample) {
           color: otuIDs,
           colorscale: "Earth"
         }
-      }];
-    
+      }]
+      
+      // set layout for bubble chart
+      let layout = {
+        xaxis:{title: "OTU ID"},
+        yaxis:{title: "Count of Bacteria"}
+      }
+
       // plot in the designated spot in index.html
-      Plotly.newPlot("bubble", bubbleData);
-  
+      Plotly.newPlot("bubble", bubbleData, layout)
   })
 }
 
@@ -135,21 +142,21 @@ function buildDemographics(sample) {
     
       // Grab the demographic info data
       let demoDataAll = data.metadata
-      // filter the data for the specific sample
+      // filter the data for the specific sample (console log for debug)
       let demographicData = demoDataAll.filter(dData => dData.id == sample)[0]
-
-      console.log("demographic info: )", demographicData)
+      console.log("demographic info:", demographicData)
+      // Create an array of the keys and values (console log for debug)
       let demoLabels = Object.keys(demographicData)
       let demoData = Object.values(demographicData)
-      console.log("demo labels", demoLabels)
-      console.log("demo data", demoData)
+      console.log("demo labels:", demoLabels)
+      console.log("demo data:", demoData)
 
       // add data under Demographic info h3
-      let demoBody = d3.select('#sample-metadata')
+      let demoBody = d3.select("#sample-metadata")
       
       for (i=0; i < demoLabels.length; i++){
-          // Append one itemper label and data combo
-          // I chose paragraph to have spacing between each (vs. body) without idention (vs. list)
+          // Append one item per label and data combo
+          // I chose paragraph to have spacing between each (vs. body) without idention (vs. unordered list)
           demoBody.append("p").text(`${demoLabels[i]}: ${demoData[i]}`)
       }
   
